0c0117e473cc2043c88401eec1c5e4433dd85751
mtmar <moto.perin@gmail.com>
2021-06-02 10:31:53 +0900

    create concept
    conseptの完成です。レスポンシブ対応も可能にしました。

--- src/components/Concept.js
+++ src/components/Concept.js
@@ -1,7 +1,27 @@
 import React from "react";
+import "../assets/styles/concept.css";
+import Model1 from "../assets/img/model1.jpeg";
+import Model2 from "../assets/img/model2.jpeg";
 
 const Concept = () => {
-  return <div>コンセプトだよ</div>;
+  return (
+    <section id="concept">
+      <div className="concept-main">
+        <p className="title">Concept</p>
+        <div className="concept-img">
+          <img className="model1" src={Model1} alt="" />
+          <img className="model2" src={Model2} alt="" />
+        </div>
+        <div className="concept-wrap">
+          <h2 className="concept-theme">Men'sExpart</h2>
+          <p className="concept-desc">
+            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストキストテキストテキストテキストテキストテキストテキストテキストテキスト
+            テキストテキストテキストテキストテキストテキスト
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Concept;
